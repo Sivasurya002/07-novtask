4ee2da11546c4b696f0026de19316b1e8e105eb1
Sivasurya002 <114906231+Sivasurya002@users.noreply.github.com>
2023-11-08 17:14:31 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 
-javac *.java
+javac file1.java
 java file1
+
+javac file2.java
 java file2
+
+javac file3.java
 java file3
+
+javac file4.java
 java file4
+
+javac file5.java
 java file5
